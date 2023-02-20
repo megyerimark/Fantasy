e561654eb97b5917d687589881cbf4441e3e1344
Megyeri Márk Máté <megyko1234@gmail.com>
2023-02-20 08:42:40 +0100

    Adatok megjelenítése

---
+++ desktop/src/controllers/MainController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import views.MainView;
+
+public class MainController {
+    MainView mainView;
+    
+    public MainController() 
+    {
+        this.mainView = new MainView();
+        this.showData();();
+    }
+
+    private void showData() {
+    }
+}
