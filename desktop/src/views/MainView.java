bf66f9d7a3347f4561d27d12bb771c2c28c96399
Megyeri Márk Máté <megyko1234@gmail.com>
2023-02-20 08:30:15 +0100

    Models

---
+++ desktop/src/views/MainView.java
@@ -0,0 +1,16 @@
+package views;
+
+import java.util.ArrayList;
+
+public class MainView {
+
+    public MainView()
+    {}
+    
+    public void show(ArrayList<Fantasy>fatn){
+        Fantasy[] fatns;
+        for (Fantasy fantasy: fatns )
+    {
+
+    }    }
+}
---
+++ desktop/src/views/models/Restapi.java
@@ -0,0 +1,7 @@
+package views.models;
+
+public class Restapi {
+    public Restapi(){
+        
+    }
+}
